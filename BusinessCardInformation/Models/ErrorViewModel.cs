f29c28ee12e50deec2bcc40f947167575036d0bd
mostafa almomani <momanimostafa98@gmail.com>
2024-10-03 22:56:14 +0300

    initial add API project

---
+++ BusinessCardInformation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessCardInformation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

e4def82f4c0b701f1d1e3052302b841fbd161594
mostafa almomani <momanimostafa98@gmail.com>
2024-10-06 17:33:46 +0300

    update BusinessCard Entity and add requierd Data Annotations

--- BusinessCardInformation.Core/Entities/BusinessCard.cs
+++ BusinessCardInformation.Core/Entities/BusinessCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,26 @@ namespace BusinessCardInformation.Core.Entities
 {
     public class BusinessCard : BaseEntity
     {
-        public int Id { get; set; }
+
+        [Required]  
         public string Name { get; set; }
+
+        [Required]  
         public string Gender { get; set; }
+
+        [Required]  
         public DateTime DateOfBirth { get; set; }
+
+        [Required]  
+        [EmailAddress]  
         public string Email { get; set; }
+
+        [Required]  
         public string Phone { get; set; }
+
+        [Required]  
         public string Address { get; set; }
-        public string PhotoBase64 { get; set; } // Optional photo in base64
+
+        public string PhotoBase64 { get; set; } 
     }
 }

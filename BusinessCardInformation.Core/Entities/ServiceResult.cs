77b81e6cfe056d2b24380171d57414b839c8d672
mostafa almomani <momanimostafa98@gmail.com>
2024-10-06 19:44:14 +0300

    Add new files ExceptionMiddleware and ServiceResult

---
+++ BusinessCardInformation.Core/Entities/ServiceResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCardInformation.Core.Entities
+{
+    public class ServiceResult<T>
+    {
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool IsSuccess => string.IsNullOrEmpty(ErrorMessage);
+
+        public ServiceResult(T data)
+        {
+            Data = data;
+        }
+
+        public ServiceResult(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+
+}
---
+++ BusinessCardInformation/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,50 @@
+﻿namespace BusinessCardInformation.Middlewares
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context); 
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred. Please try again later.",
+                Details = ex.Message 
+            };
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+    }
+
+}

4804641ea1222976c0d3589a0c975df7f3449d6b
mostafa almomani <momanimostafa98@gmail.com>
2024-10-08 19:46:01 +0300

    some of refactoring and changes for the app

--- Business-Card-Information-Web-App/src/app/app.routes.ts
+++ Business-Card-Information-Web-App/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
+import { PreloadAllModules } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+
   { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
